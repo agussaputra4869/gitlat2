a4d4d20af286713661c2c2b7c56552eab459e4ed
Agus Saputra <takehikoboyz@gmail.com>
2012-07-17 15:13:52 +0700

    AS

--- config.php
+++ config.php
@@ -1,20 +1,40 @@
-<?php
-/* ******************************** 
-   *** General Configuration    ***
-   *** Author Asutarko          ***
-   *** Versi 2.0.0              ***
-   ******************************** */
-
-include "db_config.php";
-
-
-//Requirement
-include INC_DIR."/debug.php";
-include INC_DIR."/functions.php";
-include INC_DIR."/class_database.php";
-include INC_DIR."/class_user.php";
-$users=new user;
-$cek=$users->user_check_cookie();
- 
-$database = new database();
+<?php
+
+/* ******************************** 
+
+   *** General Configuration    ***
+
+   *** Author Asutarko          ***
+
+   *** Versi 2.0.0              ***
+
+   ******************************** */
+
+
+
+include "db_config2.php";
+
+
+
+
+
+
+//Requirement
+
+include INC_DIR."/debug.php";
+
+include INC_DIR."/functions.php";
+
+include INC_DIR."/class_database.php";
+
+include INC_DIR."/class_user.php";
+
+$users=new user;
+
+$cek=$users->user_check_cookie();
+
+ 
+
+$database = new database();
+
 ?> 
